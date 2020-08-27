7a919bd3ee8970634252fa8e678455ca50b786d7
Neoministein <acertartoni@gmail.com>
2020-08-27 23:11:10 +0200

    -added a JSON reader

---
+++ src/main/java/com/neoutil/json/JSONReader.java
@@ -0,0 +1,15 @@
+package com.neoutil.json;
+
+import com.neoutil.file.FileReader;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONReader {
+    public static JSONArray readToJSONArray(String location) {
+        return new JSONArray(FileReader.readFileToString(location));
+    }
+
+    public static JSONObject readToJSONObject(String location) {
+        return new JSONObject(FileReader.readFileToString(location));
+    }
+}
