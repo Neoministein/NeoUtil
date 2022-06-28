649ab2026420f05bd1ea1e39a81c673c207a6f05
Sandro Tartoni <sandro.tartoni@swisslog.com>
2022-06-28 22:59:43 +0200

    added a basic dummy impl for SearchRepostiory

---
+++ framework/api/src/main/java/com/neo/util/framework/impl/persistence/search/PretendSearchRepository.java
@@ -0,0 +1,76 @@
+package com.neo.util.framework.impl.persistence.search;
+
+import com.neo.util.framework.api.persistence.search.*;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+@SuppressWarnings("java:S1186") //Default search implementation that does nothing
+public class PretendSearchRepository implements SearchRepository {
+
+    @Override
+    public void index(Searchable searchable) {
+
+    }
+
+    @Override
+    public void index(Searchable searchable, IndexParameter indexParameter) {
+
+    }
+
+    @Override
+    public void index(List<? extends Searchable> searchableList) {
+
+    }
+
+    @Override
+    public void index(List<? extends Searchable> searchableList, IndexParameter indexParameter) {
+
+    }
+
+    @Override
+    public void update(Searchable searchable, boolean partial) {
+
+    }
+
+    @Override
+    public void update(Searchable searchable, boolean partial, IndexParameter indexParameter) {
+
+    }
+
+    @Override
+    public void update(List<? extends Searchable> searchableList, boolean partial) {
+
+    }
+
+    @Override
+    public void delete(Searchable searchable) {
+
+    }
+
+    @Override
+    public void delete(List<? extends Searchable> searchableList) {
+
+    }
+
+    @Override
+    public void deleteAll(Class<? extends Searchable> searchableClazz) {
+
+    }
+
+    @Override
+    public SearchResult fetch(String index, SearchQuery parameters) {
+        return null;
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
+    @Override
+    public boolean enabled() {
+        return false;
+    }
+}
