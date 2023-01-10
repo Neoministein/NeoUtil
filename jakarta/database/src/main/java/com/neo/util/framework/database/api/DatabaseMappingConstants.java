26370de84021a742aebcf429559ffdee020bd7d7
Neoministein <acertartoni@gmail.com>
2023-01-10 23:06:30 +0100

    Add data constants for easier schema generation

---
+++ jakarta/database/src/main/java/com/neo/util/framework/database/api/DatabaseMappingConstants.java
@@ -0,0 +1,6 @@
+package com.neo.util.framework.database.api;
+
+public interface DatabaseMappingConstants {
+
+    short VARCHAR = 255;
+}
---
+++ jakarta/elastic/src/main/java/com/neo/util/framework/elastic/api/ElasticMappingConstants.java
@@ -0,0 +1,22 @@
+package com.neo.util.framework.elastic.api;
+
+public interface ElasticMappingConstants {
+
+    int KEYWORD = 256;
+
+    long LONG_MIN = Long.MIN_VALUE;
+
+    long LONG_MAX = Long.MAX_VALUE;
+
+    int INT_MIN = Integer.MIN_VALUE;
+
+    int INT_MAX = Integer.MAX_VALUE;
+
+    short SHORT_MIN = Short.MIN_VALUE;
+
+    short SHORT_MAX = Short.MAX_VALUE;
+
+    byte BYTE_MIN = Byte.MIN_VALUE;
+
+    byte BYTE_MAX = Byte.MAX_VALUE;
+}
