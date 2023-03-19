698b946289718cbf4f748177d220817a36a6efcb
Neoministein <acertartoni@gmail.com>
2023-03-19 23:21:34 +0100

    Add Javadoc for ServerCacheControl

--- jakarta/rest/src/main/java/com/neo/util/framework/rest/api/cache/ServerCacheControl.java
+++ jakarta/rest/src/main/java/com/neo/util/framework/rest/api/cache/ServerCacheControl.java
@@ -9,12 +9,19 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-
+/**
+ * Can be added to any Jax Rs resource method or class to cache the response server side
+ * <p>
+ * - Only occurs if the status code is 200
+ */
 @NameBinding
 @Retention(RUNTIME)
 @Target({TYPE, METHOD})
 public @interface ServerCacheControl {
 
+    /**
+     * Time in seconds after the cache should no longer be valid
+     */
     long expireAfter() default EXPIRE_AFTER_DISABLED;
 
     long EXPIRE_AFTER_DISABLED = -1;
