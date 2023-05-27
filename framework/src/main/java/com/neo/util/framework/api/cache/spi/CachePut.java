30c8a3d1bb045b3032de08090297feaba40eccf1
Neoministein <acertartoni@gmail.com>
2023-05-27 16:30:29 +0200

    Add Javadoc

--- framework/src/main/java/com/neo/util/framework/api/cache/spi/CachePut.java
+++ framework/src/main/java/com/neo/util/framework/api/cache/spi/CachePut.java
@@ -1,5 +1,6 @@
 package com.neo.util.framework.api.cache.spi;
 
+import com.neo.util.framework.api.cache.Cache;
 import com.neo.util.framework.api.cache.CacheKeyGenerator;
 import jakarta.enterprise.util.Nonbinding;
 import jakarta.interceptor.InterceptorBinding;
@@ -9,6 +10,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * A method annotated with {@link CachePut} is invoked when called from outside the current bean, a cache
+ * key will be computed and use it to try to remove an existing entry from the cache.
+ * <p>
+ * The cache key is computed using the following logic:
+ * <ul>
+ * <li>If a {@link CacheKeyGenerator} is specified with this annotation, then it is used to generate the cache key.
+ * The {@link CacheKeyParameterPositions} will then be ignored. </li>
+ * <li>Otherwise, if the method has no arguments, then the caches {@link Cache#getName()} will be used</li>
+ * <li>Otherwise, if the method has exactly one argument, then that argument is the cache key.</li>
+ * <li>Otherwise, if the method has {@link CacheKeyParameterPositions} the defined parameter positions will then be used.</li>
+ * <li>Otherwise, the cache key is an instance of {@link CompositeCacheKey} built from all the method arguments.</li>
+ * </ul>
+ * <p>
+ * This annotation can be combined with multiple other caching annotations on a single method. Caching operations will always
+ * be executed in the same order: {@link CacheInvalidateAll} first, then {@link CacheInvalidate},
+ * then {@link CachePut} and finally {@link CacheResult}.
+ */
 @InterceptorBinding
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
