92ae6e5b8c3810dbfbf9b310acb1ff7b4c50a15f
Neoministein <acertartoni@gmail.com>
2023-09-06 17:52:51 +0200

    Added admin resource for json schemas

---
+++ jakarta/rest/src/main/java/com/neo/util/framework/rest/web/rest/JsonSchemaResource.java
@@ -0,0 +1,42 @@
+package com.neo.util.framework.rest.web.rest;
+
+import com.neo.util.common.impl.exception.ExceptionDetails;
+import com.neo.util.common.impl.exception.NoContentFoundException;
+import com.neo.util.framework.impl.json.JsonSchemaLoader;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@ApplicationScoped
+@Path(JsonSchemaResource.RESOURCE_LOCATION)
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+public class JsonSchemaResource {
+
+    public static final String RESOURCE_LOCATION = "admin/api/json-schema";
+
+    public static final ExceptionDetails EX_SCHEMA_DOES_NOT_EXIST = new ExceptionDetails("common/json/schema/invalid-path",
+            "The provided json schema path [{0}] does not exist.", false);
+
+    @Inject
+    protected JsonSchemaLoader jsonSchemaLoader;
+
+    @GET
+    public List<String> getSchemaNames() {
+        return jsonSchemaLoader.getUnmodifiableMap().keySet().stream().sorted().toList();
+    }
+
+    @GET
+    @Path("{path : .+}")//This param'{path : .+}' means that it also includes '/'
+    public String getSchema(@PathParam("path") String path) {
+        return jsonSchemaLoader.getJsonSchema(path)
+                .orElseThrow(() -> new NoContentFoundException(EX_SCHEMA_DOES_NOT_EXIST, path))
+                //Substring +6 is done since default toString start with '"#" :' which isn't valid json
+                .toString().substring(6);
+    }
+}
