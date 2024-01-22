fcd5ecb28414cc3fea95c3bf1bbe8999d71e3858
Neoministein <acertartoni@gmail.com>
2024-01-23 00:02:43 +0100

    Try out a few things

---
+++ jakarta/rest/src/main/java/com/neo/util/framework/rest/web/rest/CORSFilter.java
@@ -0,0 +1,23 @@
+package com.neo.util.framework.rest.web.rest;
+
+import com.neo.util.framework.rest.api.response.ResponseGenerator;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+            final ContainerResponseContext containerRequest) {
+        containerRequest.getHeaders().add("Access-Control-Expose-Headers", ResponseGenerator.VALID_BACKEND_ERROR);
+        containerRequest.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerRequest.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, hx-request, hx-current-url");
+        containerRequest.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerRequest.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerRequest.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+
+}
